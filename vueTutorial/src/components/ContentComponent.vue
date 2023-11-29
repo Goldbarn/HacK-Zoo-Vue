08f69ab6071c5f351bfafc6fc71140deca377c7e
Goldbarn <152343413+Goldbarn@users.noreply.github.com>
2023-11-28 19:15:56 -0500

    Add files via upload

---
+++ vueTutorial/src/components/ContentComponent.vue
@@ -0,0 +1,46 @@
+<template>
+
+  <template v-if="!state.saidhi">
+  <h1>Story Ideas</h1>
+  <h2>By {{fname}} {{lname}}</h2>
+  <ul>
+    <li>Bio World</li>
+    <li>Geo World</li>
+    <li>Ritual World</li>
+    <li>{{idea}}</li>
+  </ul>
+  <button @click="state.saidhi=!state.saidhi">Say Hi?</button>
+  <p v-if="state.saidhi">Thanks!</p>
+  </template>
+
+  <template v-else>
+    <h2>WELCOME TO THE JUNGLE!</h2>
+    <button @click="state.saidhi=!state.saidhi">Note stories?</button>
+    <PettingZooAnimal/>
+    <PettingZooAnimal/>
+  </template>
+
+</template>
+
+<script setup>
+
+// JavaScript for interactive content goes here
+const fname = "Rowan";
+const lname = "Ess";
+//const idea = prompt("What is your idea")
+import {reactive} from "vue";
+const state = reactive({
+  saidhi: false
+});
+import PettingZooAnimal from "./PettingZooAnimal.vue"
+
+</script>
+
+<style scoped>
+h1 {
+  color:navy;
+}
+* {
+  font-family: sans-serif;
+}
+</style>
---
+++ vueTutorial/src/components/PettingZooAnimal.vue
@@ -0,0 +1,20 @@
+<template>
+<img @click="state.timesPet++" :src="state.imageURL"/>
+<p>Times the dangerous animal was pet: {{state.timesPet}}</p>
+</template>
+
+<script setup>
+import {reactive, watch} from "vue";
+const state = reactive({
+  timesPet: 0,
+  imageURL: "https://hacksu.com/animals/alpaca.jpg"
+})
+
+function maybewriting(){
+  if(Math.random() < 0.1){
+    state.imageURL = "https://hacksu.com/animals/alpaca-fire.gif"
+  }
+}
+
+watch(state, maybewriting)
+</script>
